7469d678bb860e5197c26901a30c4cc087df83d0
Sebastian Perez <spezuluaga@gmail.com>
2024-04-11 15:55:21 -0500

    ft/working-with-handlers

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 
 import telebot
 from dotenv import load_dotenv
+from telebot import types
 
 # Load env
 load_dotenv()
@@ -19,6 +20,32 @@ HELP_MESSAGE = """Sure, you there are a few commands that you can use:
 /horoscope
 """
 
+MENU_OPTIONS = {
+    "DOWNLOAD": "menu_download",
+    "HOROSCOPE": "menu_horoscope",
+}
+
+
+@bot.message_handler(commands=['commands-info'])
+def send_menu(message):
+    markup = types.InlineKeyboardMarkup(row_width=2)
+
+    button_download = types.InlineKeyboardButton("Download video", callback_data=MENU_OPTIONS["DOWNLOAD"])
+    button_horoscope = types.InlineKeyboardButton("Get horoscope", callback_data=MENU_OPTIONS["HOROSCOPE"])
+
+    markup.add(button_download, button_horoscope)
+
+    bot.send_message(message.chat.id, "What do you want to do?", reply_markup=markup)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    if call.data == MENU_OPTIONS["HOROSCOPE"]:
+        return bot.answer_callback_query(call.id, "You can see horoscope using /horoscope {your sign}!")
+    if call.data == MENU_OPTIONS["DOWNLOAD"]:
+        return bot.answer_callback_query(call.id, "For download you need to put /download {url}")
+    return bot.answer_callback_query(call.id, "Ops, something went wrong :/")
+
 
 # Setting commands actions (start, help, download)
 @bot.message_handler(commands=['start'])
@@ -29,6 +56,12 @@ def send_getting(message):
 @bot.message_handler(commands=['help'])
 def send_help(message):
     bot.reply_to(message, HELP_MESSAGE)
+
+
+@bot.message_handler(commands=['random-img'])
+def send_random_img(message):
+    img_url = "https://source.unsplash.com/random"
+    bot.send_photo(chat_id=message.chat.id, photo=img_url, caption="Getting random image")
 
 
 @bot.message_handler(func=lambda m: True)
